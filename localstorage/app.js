b7311a33a3cba97f4778f4d59f2e6dc47f2091ae
caiocardoso <caiocardoso78@gmail.com>
2022-10-13 21:55:27 -0300

    feat: save to local storage

--- localstorage/app.js
+++ localstorage/app.js
@@ -1,8 +1,19 @@
 const form = document.querySelector('form');
-form.addEventListener('submit', (event)=>{
+form.addEventListener('submit', (event) => {
 event.preventDefault();
 const form = event.target;
 const formData = {
 };
 console.log({formData});
+window.localStorageç.setItem('key', form.key.value);
+window.localStorage.setItem('value', form.value.value);
+readFromStorage();
 });
+
+function readFromStorage(){
+    const key = window.localStorage.getItem('key');
+    const value = window.localStorage.getItem('value');
+    document.querySelector('output')
+    .textContent = JSON.stringify( {key, value}, null, 2);
+}
+readFromStorage();
